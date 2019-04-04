8d805ec08368dd22d7fee238c230523405bb1f93
Xiaoyang Ding <x37ding@edu.uwaterloo.ca>
2019-04-04 02:11:54 -0400

    wip -- need sample data

---
+++ database_setup.sql
@@ -0,0 +1,114 @@
+CREATE DATABASE  IF NOT EXISTS `ece356_proj`;
+USE `ece356_proj`;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `Users`
+-- ----------------------------
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `UserID` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Birthday` date DEFAULT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `Posts`
+-- ----------------------------
+DROP TABLE IF EXISTS `Posts`;
+CREATE TABLE `Posts` (
+  `PostID` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Type` varchar(255) DEFAULT NULL,
+  `Content` varchar(255) DEFAULT NULL,
+  `CreatedBy` date DEFAULT NULL,
+  PRIMARY KEY (`PostID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `UserGroups`
+-- ----------------------------
+DROP TABLE IF EXISTS `UserGroups`;
+CREATE TABLE `UserGroups` (
+  `GroupID` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `CreatedBy` date DEFAULT NULL,
+  PRIMARY KEY (`GroupID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `Topics`
+-- ----------------------------
+DROP TABLE IF EXISTS `Topics`;
+CREATE TABLE `Topics` (
+  `TopicID` varchar(255) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`TopicID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `UserFollowsUser`
+-- ----------------------------
+DROP TABLE IF EXISTS `UserFollowsUser`;
+CREATE TABLE `UserFollowsUser` (
+  `UserID` varchar(255) NOT NULL,
+  `FollowUserID` varchar(255) NOT NULL,
+  `LastReadPost` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`UserID`,`FollowUserID`),
+  FOREIGN KEY (`UserID`) REFERENCES  Users(`UserID`),
+  FOREIGN KEY (`FollowUserID`) REFERENCES  Users(`UserID`),
+  FOREIGN KEY (`LastReadPost`) REFERENCES  Posts(`PostID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `UserFollowsTopic`
+-- ----------------------------
+DROP TABLE IF EXISTS `UserFollowsTopic`;
+CREATE TABLE `UserFollowsTopic` (
+  `UserID` varchar(255) NOT NULL,
+  `FollowTopicID` varchar(255) NOT NULL,
+  `LastReadPost` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`UserID`,`FollowTopicID`),
+  FOREIGN KEY (`UserID`) REFERENCES  Users(`UserID`),
+  FOREIGN KEY (`FollowTopicID`) REFERENCES  Topics(`TopicID`),
+  FOREIGN KEY (`LastReadPost`) REFERENCES  Posts(`PostID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `UserJoinGroup`
+-- ----------------------------
+DROP TABLE IF EXISTS `UserJoinGroup`;
+CREATE TABLE `UserJoinGroup` (
+  `UserID` varchar(255) NOT NULL,
+  `GroupID` varchar(255) NOT NULL,
+  PRIMARY KEY (`UserID`,`GroupID`),
+  FOREIGN KEY (`UserID`) REFERENCES  Users(`UserID`),
+  FOREIGN KEY (`GroupID`) REFERENCES  UserGroups(`GroupID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `PostRespPost`
+-- ----------------------------
+DROP TABLE IF EXISTS `PostRespPost`;
+CREATE TABLE `PostRespPost` (
+  `PostID` varchar(255) NOT NULL,
+  `ResponseID` varchar(255) NOT NULL,
+  PRIMARY KEY (`PostID`,`ResponseID`),
+  FOREIGN KEY (`PostID`) REFERENCES  Posts(`PostID`),
+  FOREIGN KEY (`ResponseID`) REFERENCES  Posts(`PostID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `PostUnderTopic`
+-- ----------------------------
+DROP TABLE IF EXISTS `PostUnderTopic`;
+CREATE TABLE `PostUnderTopic` (
+  `PostID` varchar(255) NOT NULL,
+  `TopicID` varchar(255) NOT NULL,
+  PRIMARY KEY (`PostID`,`TopicID`),
+  FOREIGN KEY (`PostID`) REFERENCES  Posts(`PostID`),
+  FOREIGN KEY (`TopicID`) REFERENCES  Topics(`TopicID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
